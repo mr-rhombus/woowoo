b739b800f289a1eb418b90bdf5b83ac2c481a3db
mr-rhombus <diamax98@gmail.com>
2026-08-03 07:35:37 -0400

    Make splash page mobile-friendly

---
+++ pages/splash/splash.js
@@ -0,0 +1,6 @@
+const viewportWidth = window.innerWidth;
+const bgImage = document.querySelector(".scroll-background");
+
+if (viewportWidth <= 768) {
+  bgImage.src = "/assets/img/bg1-color-tall-mobile.jpeg";
+}
