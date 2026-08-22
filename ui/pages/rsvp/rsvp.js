a7b8516eaefb55dac82a0512097138f6581ddd04
mr-rhombus <diamax98@gmail.com>
2026-08-21 21:39:35 -0400

    Programmatically render guests on rsvp page

--- ui/pages/rsvp/rsvp.js
+++ ui/pages/rsvp/rsvp.js
@@ -1,9 +1,11 @@
 const baseUrl = "http://localhost:8000";
 
 const findPartyBtn = document.querySelector(".findPartyBtn");
-findPartyBtn.addEventListener("click", findParty);
+findPartyBtn.addEventListener("click", renderParties);
 
-async function findParty(event) {
+const guestsDiv = document.querySelector(".guests");
+
+async function renderParties(event) {
   event.preventDefault();
 
   const lastNameElement = document.getElementById("lastName");
@@ -18,9 +20,94 @@ async function findParty(event) {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(lastNameData),
     });
-    const partyData = await response.json();
-    console.log(partyData);
+    const result = await response.json();
+
+    const partyIds = [...new Set(result.guests.map((guest) => guest.group_id))];
+
+    partyIds.forEach((partyId) => {
+      const partyGuests = result.guests.filter(
+        (guest) => guest.group_id === partyId,
+      );
+      const partyLastName = partyGuests.sort((a, b) =>
+        b.last_name.localeCompare(a.lastName),
+      )[0].last_name;
+      guestsDiv.appendChild(createParty(partyLastName, partyGuests));
+    });
   } catch (error) {
     console.error("Error:", error);
   }
+}
+
+function createParty(partyName, guestData) {
+  const fieldset = document.createElement("fieldset");
+
+  const legend = document.createElement("legend");
+  legend.textContent = partyName;
+
+  const partyDiv = document.createElement("div");
+  partyDiv.classList.add("party");
+
+  fieldset.appendChild(legend);
+
+  guestData.forEach((guest) => {
+    partyDiv.appendChild(createGuest(guest.full_name, guest.rsvp));
+  });
+  fieldset.appendChild(partyDiv);
+
+  return fieldset;
+}
+
+function createGuest(name, response) {
+  const guestDiv = document.createElement("div");
+  guestDiv.classList.add("guest");
+
+  const nameDiv = document.createElement("div");
+  nameDiv.classList.add("guestName");
+  nameDiv.textContent = name;
+
+  const responseDiv = document.createElement("div");
+  responseDiv.classList.add("guestResponse");
+
+  responseDiv.appendChild(createResponseRadio(name, "y", response));
+  responseDiv.appendChild(createResponseRadio(name, "n", response));
+
+  guestDiv.appendChild(nameDiv);
+  guestDiv.appendChild(responseDiv);
+
+  return guestDiv;
+}
+
+function createResponseRadio(guestName, responseOption, actualResponse) {
+  const responseDiv = document.createElement("div");
+  responseDiv.classList.add("radioResponse");
+
+  const radioInput = document.createElement("input");
+  radioInput.type = "radio";
+  radioInput.name = guestName.replace(" ", "_");
+
+  const radioLabel = document.createElement("label");
+
+  switch (responseOption.toLowerCase()) {
+    case "y":
+      radioInput.id = radioInput.name + "_y";
+      radioLabel.htmlFor = radioInput.id;
+      radioLabel.textContent = "Will Attend";
+      if (actualResponse && actualResponse.toLowerCase() === "y") {
+        radioInput.checked = true;
+      }
+      break;
+    case "n":
+      radioInput.id = radioInput.name + "_n";
+      radioLabel.htmlFor = radioInput.id;
+      radioLabel.textContent = "Will Not Attend";
+      if (actualResponse && actualResponse.toLowerCase() === "n") {
+        radioInput.checked = true;
+      }
+      break;
+  }
+
+  responseDiv.appendChild(radioInput);
+  responseDiv.appendChild(radioLabel);
+
+  return responseDiv;
 }
