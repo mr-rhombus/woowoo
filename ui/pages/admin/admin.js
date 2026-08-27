c2ae783c0fd7716ba1530e48582fdf2fcbb2270d
mr-rhombus <diamax98@gmail.com>
2026-08-26 20:25:13 -0400

    Add pwd protection to admin page

--- ui/pages/admin/admin.js
+++ ui/pages/admin/admin.js
@@ -1,6 +1,78 @@
+import { passwordIsValid } from "/ui/pages/pages.js";
+
 const baseUrl = "http://localhost:8000";
 
+const containerEl = document.querySelector(".container");
+
 const tbodyEl = document.querySelector("tbody");
+
+function renderPage() {
+  renderPwdModal();
+}
+
+function renderPwdModal() {
+  const pwdDiv = document.createElement("div");
+
+  const pwdForm = document.createElement("form");
+  pwdForm.method = "post";
+
+  const pwdInputEl = document.createElement("input");
+  pwdInputEl.type = "password";
+  pwdInputEl.name = "adminPwd";
+  pwdInputEl.id = "adminPwd";
+  pwdInputEl.placeholder = "Enter Password";
+  pwdInputEl.autofocus = true;
+
+  pwdForm.appendChild(pwdInputEl);
+
+  const submitBtn = document.createElement("button");
+  submitBtn.type = "submit";
+  submitBtn.id = "pwdSubmitBtn";
+  submitBtn.textContent = "Login";
+
+  pwdForm.appendChild(submitBtn);
+
+  pwdForm.addEventListener("submit", checkPassword);
+
+  pwdDiv.appendChild(pwdForm);
+
+  containerEl.appendChild(pwdDiv);
+}
+
+async function checkPassword(event) {
+  event.preventDefault();
+
+  const passwordInput = document.getElementById("adminPwd");
+  const submitBtn = document.getElementById("pwdSubmitBtn");
+  const isValid = await passwordIsValid("admin", passwordInput.value);
+
+  if (isValid) {
+    renderTable();
+  } else {
+    submitBtn.blur();
+
+    passwordInput.classList.add("shake");
+    passwordInput.addEventListener(
+      "animationend",
+      () => {
+        passwordInput.classList.remove("shake");
+      },
+      { once: true },
+    );
+  }
+  passwordInput.value = "";
+}
+
+async function renderTable() {
+  const response = await fetch(`${baseUrl}/api/get_all_guests`, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  });
+  const result = await response.json();
+  result.guests.forEach((guest) => {
+    tbodyEl.appendChild(createRow(guest.full_name, guest.rsvp));
+  });
+}
 
 function createRow(name, rsvp) {
   const rowEl = document.createElement("tr");
@@ -26,15 +98,4 @@ function createRow(name, rsvp) {
   return rowEl;
 }
 
-async function renderTable() {
-  const response = await fetch(`${baseUrl}/api/get_all_guests`, {
-    method: "GET",
-    headers: { "Content-Type": "application/json" },
-  });
-  const result = await response.json();
-  result.guests.forEach((guest) => {
-    tbodyEl.appendChild(createRow(guest.full_name, guest.rsvp));
-  });
-}
-
-renderTable();
+renderPage();
